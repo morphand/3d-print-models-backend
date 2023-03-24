1e689ca2755be2d44173788c84b62a39eecb3b8d
morphand <75131830+morphand@users.noreply.github.com>
2023-03-24 22:56:48 +0200

    add file upload middleware and controller

--- index.js
+++ index.js
@@ -2,11 +2,13 @@
 const express = require("express");
 const cors = require("cors");
 const mongoose = require("mongoose");
+const fileUpload = require("express-fileupload");
 
 // Controllers
 const loginController = require("./controllers/login");
 const registerController = require("./controllers/register");
 const userController = require("./controllers/user");
+const uploadController = require("./controllers/upload");
 
 const constants = require("./constants");
 
@@ -16,6 +18,12 @@ const app = express();
 // Middlewares
 app.use(cors());
 app.use(express.json());
+app.use(
+  // Set max file size to upload to ~50MB.
+  fileUpload({
+    limits: { fileSize: 50 * 1024 * 1024 },
+  })
+);
 
 mongoose.connect(`mongodb://127.0.0.1/${constants.DB_NAME}`).then(() => {
   console.log(`Connected to database ${constants.DB_NAME}.`);
@@ -32,3 +40,6 @@ app.post("/register", registerController);
 
 // User API endpoint
 app.get("/api/user/:id", userController);
+
+// Upload endpoint
+app.post("/api/user/:id/upload", uploadController);
